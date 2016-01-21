bec66d84cee5bb85e091599e8e04ae0d19903c3a
Leon <leonjiang1121@gmail.com>
2016-01-21 16:42:26 +0800

    use morgan

--- connect.js
+++ connect.js
@@ -1,6 +1,20 @@
 var connect = require('connect');
+/**
+ * morgan-the previous logger.
+ * 		  now become individual.
+ */
+var morgan = require('morgan');
+/**
+ * 	dev 日志格式 
+ */
+var logger = morgan('dev');
+/**
+ * 程序开始
+ */
 var app = connect()
+    .use(logger)
     .use(function (req, res) {
         res.end('hello world\n');
     })
     .listen(3000);
+
